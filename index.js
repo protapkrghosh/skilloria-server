27c554f7c59832b2e44ac000cbf89be15cd6db9d
protapkrghosh <protapg518@gmail.com>
2025-09-18 23:41:50 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(process.env.SKILLORIA_MONGO_URI, {
+   serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+   },
+});
+
+async function run() {
+   try {
+      // await client.connect();
+
+      await client.db("admin").command({ ping: 1 });
+      console.log(
+         "Pinged your deployment. You successfully connected to MongoDB!"
+      );
+   } finally {
+      // await client.close();
+   }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+   res.send("Skilloria is running...");
+});
+
+app.listen(port, () => {
+   console.log(`Skilloria running on port ${port}`);
+});
